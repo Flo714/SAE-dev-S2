b5b3b3a2af73631bf8d11487cd26288b6d5e0f21
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-30 21:15:02 +0200

    CSS - Page Mentions Légales

--- src/views/MentionlegalView.vue
+++ src/views/MentionlegalView.vue
@@ -1,27 +1,27 @@
 <template>
-    <h1>Mentions légales</h1>
+    <h1 class="pt-8">Mentions légales</h1>
 
-    <h2>1. Présentation du site internet</h2>
+    <h2 class="my-6">1. Présentation du site internet</h2>
 
-    <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://tradi-val.fr l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:</p>
-    <p>Propriétaire : Demortiere – 25200 25200 Montbéliard
+    <p class="my-2">En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://tradi-val.fr l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:</p>
+    <p class="my-2">Propriétaire : Demortiere – 25200 25200 Montbéliard
 Responsable publication : https://tradi-val.fr
 Le responsable publication est une personne physique ou une personne morale.
 Webmaster : Demortiere – https://tradi-val.fr
 Hébergeur : ovh – 2 rue Kellermann 59100 Roubaix 1007</p>
-    <p>Délégué à la protection des données : Demortiere – florentin.demortiere@edu.univ-fcomte.fr
+    <p class="my-2">Délégué à la protection des données : Demortiere – florentin.demortiere@edu.univ-fcomte.fr
 Ce modèle de mentions légales est proposé par le générateur gratuit de mentions légales pour un site internet</p>
 
-    <h2>2. Conditions générales d’utilisation du site et des services proposés</h2>
+    <h2 class="my-6">2. Conditions générales d’utilisation du site et des services proposés</h2>
 
-    <p>Le Site constitue une œuvre de l’esprit protégée par les dispositions du Code de la Propriété Intellectuelle et des Réglementations Internationales applicables. Le Client ne peut en aucune manière réutiliser, céder ou exploiter pour son propre compte tout ou partie des éléments ou travaux du Site.</p>
-    <p>L’utilisation du site https://tradi-val.fr implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du site https://tradi-val.fr sont donc invités à les consulter de manière régulière.</p>
-    <p>Ce site internet est normalement accessible à tout moment aux utilisateurs. Une interruption pour raison de maintenance technique peut être toutefois décidée par https://tradi-val.fr, qui s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention. Le site web https://tradi-val.fr est mis à jour régulièrement par https://tradi-val.fr responsable. De la même façon, les mentions légales peuvent être modifiées à tout moment : elles s’imposent néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent possible afin d’en prendre connaissance.</p>
+    <p class="my-2">Le Site constitue une œuvre de l’esprit protégée par les dispositions du Code de la Propriété Intellectuelle et des Réglementations Internationales applicables. Le Client ne peut en aucune manière réutiliser, céder ou exploiter pour son propre compte tout ou partie des éléments ou travaux du Site.</p>
+    <p class="my-2">L’utilisation du site https://tradi-val.fr implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du site https://tradi-val.fr sont donc invités à les consulter de manière régulière.</p>
+    <p class="my-2">Ce site internet est normalement accessible à tout moment aux utilisateurs. Une interruption pour raison de maintenance technique peut être toutefois décidée par https://tradi-val.fr, qui s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention. Le site web https://tradi-val.fr est mis à jour régulièrement par https://tradi-val.fr responsable. De la même façon, les mentions légales peuvent être modifiées à tout moment : elles s’imposent néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent possible afin d’en prendre connaissance.</p>
 
-    <h2>3. Description des services fournis</h2>
+    <h2 class="my-6">3. Description des services fournis</h2>
 
-    <p>Le site internet https://tradi-val.fr a pour objet de fournir une information concernant l’ensemble des activités de la société. https://tradi-val.fr s’efforce de fournir sur le site https://tradi-val.fr des informations aussi précises que possible. Toutefois, il ne pourra être tenu responsable des oublis, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.</p>
-    <p>Toutes les informations indiquées sur le site https://tradi-val.fr sont données à titre indicatif, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site https://tradi-val.fr ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne</p>
+    <p class="my-2">Le site internet https://tradi-val.fr a pour objet de fournir une information concernant l’ensemble des activités de la société. https://tradi-val.fr s’efforce de fournir sur le site https://tradi-val.fr des informations aussi précises que possible. Toutefois, il ne pourra être tenu responsable des oublis, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.</p>
+    <p class="my-2 pb-6">Toutes les informations indiquées sur le site https://tradi-val.fr sont données à titre indicatif, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site https://tradi-val.fr ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne</p>
 
 </template>
 
