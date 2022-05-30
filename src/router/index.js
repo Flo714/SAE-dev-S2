38e0d5a11c20503937457b2cb2c41cd64d0d7ddc
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-30 09:31:05 +0200

    HTML - Page Artistes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/AccueilView.vue'
 import ProgrammeView from '../views/ProgrammeView.vue'
+import ArtistesView from '../views/ArtistesView.vue'
 
 
 const router = createRouter({
@@ -8,6 +9,7 @@ const router = createRouter({
   routes: [
     { path: '/', name: 'AccueilView', component: AccueilView },
     { path: '/Programme', name: 'ProgrammeView', component: ProgrammeView },
+    { path: '/Artistes', name: 'ArtistesView', component: ArtistesView },
 
     // ici les autres routes
   ]
---
+++ src/views/ArtistesView.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Les artistes</h1>
+
+    <div>
+        <!-- Card Artiste -->
+    </div>
+</template>
+
+<script>
+// import  from ""
+
+export default {
+  name: "App",
+  components: {  },
+};
+</script>
