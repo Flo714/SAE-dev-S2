39d7a524eaa7642dc9dfa60ebf489b8170f5535d
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-30 20:40:41 +0200

    CSS - Page Festival

--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -1,34 +1,34 @@
 <template>
     <section>
-        <h1>Le Festival</h1>
+        <h1 class="py-8">Le Festival</h1>
 
-        <p>Le Tradi’Val est un festival de chansons traditionnelles qui se déroule à Germagny, en Saône et Loire. Venez nous rejoindre le 5, 6 et 7 Août 2022</p>
+        <p class="mr-6">Le Tradi’Val est un festival de chansons traditionnelles qui se déroule à Germagny, en Saône et Loire. Venez nous rejoindre le 5, 6 et 7 Août 2022</p>
     </section>
     <section>
-        <h2>Le thème</h2>
+        <h2 class="my-6">Le thème</h2>
 
-        <img src="../../public/img-squirrel/img-festival.webp" alt="Image du festival">
+        <img class="w-[75%] block m-auto rounded-xl" src="../../public/img-squirrel/img-festival.webp" alt="Image du festival">
 
-        <p>Le Tradi’Val est un festival familial et convivial. Venez avec vos familles ou vos amis pour écouter les merveilleuses musiques traditionnelles et paillarde de la région.</p>
+        <p class="my-4">Le Tradi’Val est un festival familial et convivial. Venez avec vos familles ou vos amis pour écouter les merveilleuses musiques traditionnelles et paillarde de la région.</p>
     </section>
     <section>
-        <h2>Où se situe le Festival ?</h2>
+        <h2 class="my-6">Où se situe le Festival ?</h2>
 
-        <!-- Carte -->
+        <iframe class="w-[100%] h-72" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2737.7129060004945!2d4.6004732154738095!3d46.67192897913383!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f3abec6e14bfbd%3A0xc214cc374917eb8a!2sTradi&#39;Val!5e0!3m2!1sfr!2sfr!4v1653920753207!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-        <p>Germagny est un petit village qui se situe entre le canton de la côte chalonnaise et le charollais. Il se situe à 30 min de Châlon sur Saône et à 50 min de Mâcon.</p>
+        <p class="my-4">Germagny est un petit village qui se situe entre le canton de la côte chalonnaise et le charollais. Il se situe à 30 min de Châlon sur Saône et à 50 min de Mâcon.</p>
     </section>
     <section>
-        <h2>Comment accéder au  Festival ?</h2>
+        <h2 class="my-4">Comment accéder au  Festival ?</h2>
 
-        <p>Germagny :</p>
+        <p class="font-semibold">Germagny :</p>
 
-        <img src="../../public/img-squirrel/Carte-Germagny.webp" alt="Carte de Germagny">
+        <img class="w-[75%] block m-auto rounded-xl my-4" src="../../public/img-squirrel/Carte Germagny.webp" alt="Carte de Germagny">
 
-        <p>Le Tradi’Val se déroule au niveau du lavoir de Germagny au bord de la rivière de la Guye. Pour vous garer, vous pouvez utiliser le pré mis à disposition.</p>
+        <p class="my-6">Le Tradi’Val se déroule au niveau du lavoir de Germagny au bord de la rivière de la Guye. Pour vous garer, vous pouvez utiliser le pré mis à disposition.</p>
     </section>
 
-    <p>Alors plus d’excuse, venez au Tradi’Val !</p>
+    <p class="font-semibold text-right py-4">Alors plus d’excuse, venez au Tradi’Val !</p>
 </template>
 
 <script>
