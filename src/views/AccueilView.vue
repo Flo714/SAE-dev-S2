200e432155cd23105250f939c54943f5de71e19f
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-30 09:03:30 +0200

    HTML - Accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,5 +1,42 @@
 <template>
+    <section>
+        <h1>Un lieu convivial et familial, c’est le Tradi’Val</h1>
 
+        <img src="" alt="">
+
+        <p>Le Tradi’Val est un festival de chansons traditionnelles qui se déroule à Germagny, en Saône et Loire. Venez nous rejoindre le 5, 6 et 7 Août 2022</p>
+    </section>
+    <section>
+        <div>
+            <p>Germagny</p>
+            <p>Saône et Loire</p>
+        </div>
+        <div>
+            <p>3 Jours de folie</p>
+        </div>
+        <div>
+            <p>Des chansons traditionnelles</p>
+        </div>
+    </section>
+    <section>
+        <h2>Le Tradi vous accueille les 5, 6 et 7 Août à Germagny !</h2>
+        
+        <p>Au Tradi’Val, nous saurons vous accueillir avec des chansons traditionnelles, des décors renversants et des magnifiques chanteurs.</p>
+        <!-- Composant bouton -->
+        <button></button>
+    </section>
+    <section>
+        <h2>Programme</h2>
+
+        <!-- Composant bouton -->
+        <button></button>
+    </section>
+    <section>
+        <h2>Les artistes</h2>
+        <p>Voici quelques artistes qui seront présents lors du Tradi’Vals</p>
+
+        <!-- Card Artiste -->
+    </section>
 </template>
 <script>
 // import  from ""
