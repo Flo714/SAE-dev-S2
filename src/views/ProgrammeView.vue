b459b45b482b57eb9dfb0132d791616c8c395c03
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-31 11:21:36 +0200

    Responsive - Page Programme (ipadmini)

--- src/views/ProgrammeView.vue
+++ src/views/ProgrammeView.vue
@@ -1,39 +1,39 @@
 <template>
-    <h1 class="py-4">La Programmation</h1>
+    <h1 class="py-4 md:text-4xl md:pt-12">La Programmation</h1>
 
     <section class="my-8">
-        <h2 class="my-4 before:bg-jaune before:absolute before:w-8 before:h-3 before:rounded"><span class="relative ml-2">Vendredi 7 Août 2022</span></h2>
-        <ul>
-            <li>18H00 - Ouverture </li>
-            <li>19H00 - Concert d’Annabelle Rogelet </li>
-            <li>21H00 - Concert du groupe : Les Mauvaises Herbes</li>
-            <li>00H00 - Fin du 1er Jour</li>
+        <h2 class="my-4 before:bg-jaune before:absolute before:w-8 before:h-3 before:rounded md:before:w-10 md:before:h-4"><span class="relative ml-2 md:text-2xl">Vendredi 7 Août 2022</span></h2>
+        <ul class="mx-6">
+            <li class="responsivetext">18H00 - Ouverture </li>
+            <li class="responsivetext">19H00 - Concert d’Annabelle Rogelet </li>
+            <li class="responsivetext">21H00 - Concert du groupe : Les Mauvaises Herbes</li>
+            <li class="responsivetext">00H00 - Fin du 1er Jour</li>
         </ul>
     </section>
     <section class="my-8">
-        <h2 class="my-4 before:bg-jaune before:absolute before:w-8 before:h-3 before:rounded"><span class="relative ml-2">Samedi 8 Août 2022</span></h2>
-        <ul class="list-none">
-            <li>10H00 - Ouverture du festival</li>
-            <li>11H00 - Concert du groupe : Les Oiseaux de Passage</li>
-            <li>13H00 - Pause déjeuner</li>
-            <li>14H00 - Concert du groupe : L'Otite Orphéonique</li>
-            <li>17H00 - Concert de Louise Jallu</li>
-            <li>20H00 - Pause dîner</li>
-            <li>21H00 - Soirée festive</li>
-            <li>00H00 - Fin du 2eme Jour</li>
+        <h2 class="my-4 before:bg-jaune before:absolute before:w-8 before:h-3 before:rounded md:before:w-10 md:before:h-4"><span class="relative ml-2 md:text-2xl">Samedi 8 Août 2022</span></h2>
+        <ul class="mx-6">
+            <li class="responsivetext">10H00 - Ouverture du festival</li>
+            <li class="responsivetext">11H00 - Concert du groupe : Les Oiseaux de Passage</li>
+            <li class="responsivetext">13H00 - Pause déjeuner</li>
+            <li class="responsivetext">14H00 - Concert du groupe : L'Otite Orphéonique</li>
+            <li class="responsivetext">17H00 - Concert de Louise Jallu</li>
+            <li class="responsivetext">20H00 - Pause dîner</li>
+            <li class="responsivetext">21H00 - Soirée festive</li>
+            <li class="responsivetext">00H00 - Fin du 2eme Jour</li>
         </ul>
     </section>
     <section class="mt-8 pb-8">
-        <h2 class="my-4 before:bg-jaune before:absolute before:w-8 before:h-3 before:rounded"><span class="relative ml-2">Dimanche 9 Août 2022</span></h2>
-        <ul>
-            <li>10H00 - Ouverture du festival</li>
-            <li>11H00 - Concert du Groupe : Crystal Duet</li>
-            <li>13H00 - Pause déjeuner</li>
-            <li>14H00 - Concert de Louise Jallu</li>
-            <li>17H00 - Concert du groupe : L'Otite Orphéonique</li>
-            <li>20H00 - Pause dîner</li>
-            <li>21H00 - Concert d’Annabelle Rogelet </li>
-            <li>00H00 - Fin du Festival</li>
+        <h2 class="my-4 before:bg-jaune before:absolute before:w-8 before:h-3 before:rounded md:before:w-10 md:before:h-4"><span class="relative ml-2 md:text-2xl">Dimanche 9 Août 2022</span></h2>
+        <ul class="mx-6">
+            <li class="responsivetext">10H00 - Ouverture du festival</li>
+            <li class="responsivetext">11H00 - Concert du Groupe : Crystal Duet</li>
+            <li class="responsivetext">13H00 - Pause déjeuner</li>
+            <li class="responsivetext">14H00 - Concert de Louise Jallu</li>
+            <li class="responsivetext">17H00 - Concert du groupe : L'Otite Orphéonique</li>
+            <li class="responsivetext">20H00 - Pause dîner</li>
+            <li class="responsivetext">21H00 - Concert d’Annabelle Rogelet </li>
+            <li class="responsivetext">00H00 - Fin du Festival</li>
         </ul>
     </section>
 </template>
